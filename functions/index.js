eff9d786f76183c706e3aeaa3d5cee4cac63bad1
milw0rm <sebastiantace@gmail.com>
2018-04-20 09:52:02 +0200

    segundo

--- functions/index.js
+++ functions/index.js
@@ -2,23 +2,23 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from tager12 tager12!");
- });
 
- exports.emojify = functions.database.ref("/messages/{pushId}/text").onCreate(myHandler =>{
-   console.log("Emojify...");
-   var originaldata = myHandler.val();
-   var emojifiedData = emojifyText(originaldata);
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!"); });
 
-   return myHandler.ref.set(emojifiedData);
+exports.emojify = functions.database.ref("/messages/{pushID}/text").onCreate(myHandler => {
+    console.log("Emojify...");
+    var originalData = myHandler.val();
+    var emojifiedData = emojifyText(originalData);
 
- });
+    return myHandler.ref.set(emojifiedData);
+});
 
- function emojifyText(text){
-   var emojifiedText = text;
-   emojifiedText = emojifiedText.replace(/\blol\b/ig, "😂");
-   emojifiedText = emojifiedText.replace(/\bcat\b/ig, "😸");
-   return emojifiedText;
- }
+function emojifyText(text) {
+    var emojifiedText = text;
+    emojifiedText = emojifiedText.replace(/\blol\b/ig, "😂");
+    emojifiedText = emojifiedText.replace(/\bcat\b/ig, "😸");
+    emojifiedText = emojifiedText.replace(/\b645\b/ig, "***");
+    return emojifiedText;
+}
+
